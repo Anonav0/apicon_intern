e0ee145b1e0a7a246460ede856e55f98d60896b3
Anonav0 <commonpeople27@gmail.com>
2021-12-11 21:14:13 +0530

    internship assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+//importing bootstrap.
+import { Container,Navbar, Button, Row, Col, Card} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//importing the icon from font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faUsers} size="4x" />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar>
+        <Container>
+          <Navbar.Toggle/>
+          <Navbar.Collapse className="justify-content-end">
+            <Button>Create security group</Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+
+        <Row>
+          <Col  md={4} className="padding">
+            <Card className="shadow" >
+              <Card.Body>
+                <Card.Title className="padding-bottom" >Admin Group</Card.Title>
+                
+                <p>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  md={4} className="padding">
+            <Card className="shadow" >
+              <Card.Body>
+                <Card.Title className="padding-bottom" >Managers</Card.Title>
+                
+                <p>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  md={4} className="padding">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title className="padding-bottom" >Sales Team</Card.Title>
+                
+                <p>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  md={4} className="padding">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title className="padding-bottom" >HR Team</Card.Title>
+                
+                <p>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  md={4} className="padding">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title className="padding-bottom">IT Team</Card.Title>
+                
+                <p s>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col  md={4} className="padding">
+            <Card className="shadow">
+              <Card.Body>
+                <Card.Title className="padding-bottom">Production Team</Card.Title>
+                
+                <p>{element}</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+      </Container>
+
     </div>
   );
 }
